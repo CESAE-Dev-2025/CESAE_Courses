e36ddb99bd9cf0baa33956cfb5e849e21cf6247d
Leandro Gabriel <github@lagabriel.com>
2026-02-05 16:55:30 +0000

    Refatora `App.tsx` para simplificar chamadas de API e remove código não utilizado

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,13 @@
 import {useState} from 'react'
 import beaver from './assets/beaver.svg'
-import type {ApiResponse} from 'shared'
 import './App.css'
-import {Course} from "shared/dist/types/course.ts";
+import {Course} from "shared/dist/types/course";
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
 
 function App() {
-    const [data, setData] = useState<ApiResponse | undefined>()
-    const [courseData, setCourseData] = useState<Course | undefined>()
     const [coursesData, setCoursesData] = useState<Course[] | undefined>()
-
-    async function sendRequest() {
-        try {
-            const req = await fetch(`${SERVER_URL}/hello`)
-            const res: ApiResponse = await req.json()
-            setData(res)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    async function getCourse(courseId: number) {
-        try {
-            const req = await fetch(`${SERVER_URL}/courses/${courseId}`)
-            const res: Course = await req.json()
-            setCourseData(res)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    // const [courseData, setCourseData] = useState<Course | undefined>()
 
     async function getCourses() {
         try {
@@ -40,6 +18,19 @@ function App() {
             console.log(error)
         }
     }
+
+    /* Função para buscar 1 curso (por id)
+    async function getCourse(courseId: number) {
+
+        try {
+            const req = await fetch(`${SERVER_URL}/courses/${courseId}`)
+            const res: Course = await req.json()
+            setCourseData(res)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    */
 
     return (
         <>
@@ -53,35 +44,24 @@ function App() {
             <p>A typesafe fullstack monorepo</p>
             <div className="card">
                 <div className='button-container'>
-                    <button onClick={sendRequest}>
-                        Call API
-                    </button>
-                    <button onClick={() => getCourse(1)}>
-                        Call API (curso 1)
-                    </button>
+                    {/*<button onClick={() => getCourse(1)}>*/}
+                    {/*    Call API (curso 1)*/}
+                    {/*</button>*/}
                     <button onClick={getCourses}>
                         Call API (cursos)
                     </button>
                     <a className='docs-link' target='_blank' href="https://bhvr.dev">Docs</a>
                 </div>
-                {data && (
-                    <pre className='response'>
-                        <code>
-                            Message: {data.message} <br/>
-                            Success: {data.success.toString()}
-                        </code>
-                      </pre>
-                )}
-                {courseData && (
-                    <pre className='response'>
-                        <code>
-                            Curso: {courseData.name} <br/>
-                            Início: {courseData.startDate} <br/>
-                            Horário: {courseData.time} / {courseData.timeDescription}<br/>
-                            Duração: {courseData.duration}
-                        </code>
-                  </pre>
-                )}
+                {/*{courseData && (*/}
+                {/*    <pre className='response'>*/}
+                {/*        <code>*/}
+                {/*            Curso: {courseData.name} <br/>*/}
+                {/*            Início: {courseData.startDate} <br/>*/}
+                {/*            Horário: {courseData.time} / {courseData.timeDescription}<br/>*/}
+                {/*            Duração: {courseData.duration}*/}
+                {/*        </code>*/}
+                {/*  </pre>*/}
+                {/*)}*/}
                 {coursesData && (
                     <pre className='response'>
                         <code>
