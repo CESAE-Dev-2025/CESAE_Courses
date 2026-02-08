6cbc4ef118f3e44bc0c201a825d25cdd75610fb6
Sandro-Draeger <sandrodraegerpt@gmail.com>
2026-02-08 22:16:04 +0000

    scraping page "curso"

--- scraping/index.js
+++ scraping/index.js
@@ -42,12 +42,51 @@ const { chromium } = require("playwright");
         await page.waitForTimeout(800);
     }
 
-    // Aqui tem todas as URLs do cursos disponiveis na pagina
+    //Aqui tem todas as URLs do cursos disponiveis na pagina
     const courseUrls = await page.$$eval("article a", links =>
         [...new Set(links.map(a => a.href))]
     );
 
     console.log(courseUrls);
 
+    //Fazer Scraping de todas as paginas de forma automatizada
+    for (const url of courseUrls) {
+        await page.goto(url, { waitUntil: "networkidle" });
+
+        const data = await page.evaluate(() => ({
+           //titulo: document.querySelector("h1, h2, h3")?.innerText,
+            //texto: document.body.innerText,
+            //imagem: document.querySelector("img")?.src,
+            name: document.querySelector("h1")?.innerText,
+            cover_url: document.querySelector("#mainSection_imgCourse")?.src ?? null,
+
+            start_date: document.querySelector("#mainSection_lblDtInicio")?.innerText,
+            end_date: document.querySelector("#mainSection_lblDtFim")?.innerText,
+            time: document.querySelector("#mainSection_lblHorario")?.innerText,
+            time_description: document.querySelector(".breadcrumb")?.innerText,
+
+            duration: document.querySelector("#mainSection_lblDuracao")?.innerText,
+            regime: document.querySelector(".breadcrumb")?.innerText,
+            location: document.querySelector("aside.signup ul li:first-child")?.innerText,
+
+            //description: document.querySelector("#66")?.innerText,
+            //audience: document.querySelector("#66")?.innerText,
+            //requirements: document.querySelector("#66")?.innerText,
+
+            //project: document.querySelector("#66")?.innerText,
+            price: document.querySelector("#mainSection_lblPrice")?.innerText,
+            //benefits: document.querySelector("#66")?.innerText,
+            //goals: document.querySelector("#66")?.innerText,
+
+            sponsor_img_url: document.querySelector('#mainSection_imgLogoParceiros')?.src ?? null,
+            //course_content: document.querySelector("#66")?.innerText,
+            //enrollment: document.querySelector("#66")?.innerText,
+
+            has_download_button: 0,
+        }));
+
+        console.log(data);
+    }
     await browser.close();
+    
 })();
