4a446b45c18fef4077136e9472b3377725010477
Leandro Gabriel <github@lagabriel.com>
2026-02-10 14:04:23 +0000

    Remove `scraper.js` and its related references, replace `node-cron` with `croner`, and cleanup unused logging code

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,24 +10,9 @@ import scheduleScraper from "./cronJob";
 import {runScrapeAndSave} from "./scraper";
 import {customLogger} from "./CustomLogger";
 
-scheduleScraper();
-
 const app = new Hono()
 
 const db = drizzle(process.env.DATABASE_URL!);
-// const logDate = () => new Date()
-//     .toISOString()
-//     .replace('Z', '')
-//     .replace('T', ' ')
-//
-// export const customLogger = (logType: string, message: string, ...rest: string[]) => {
-//     logType = logType.toUpperCase()
-//     if (logType === 'ERROR') {
-//         console.error(`${logDate()} | ${logType}:`, message, ...rest)
-//     } else {
-//         console.log(`${logDate()} | ${logType}:`, message, ...rest)
-//     }
-// }
 
 app.use(cors())
 
@@ -40,6 +25,8 @@ app.use(
 );
 
 app.use(logger())
+
+scheduleScraper();
 
 // TODO: Proteger este endpoint (token, JWT, IP whitelist, etc.)
 app.post('/admin/run-scrape', async c => {
