1f1b5c358a00830520f808bf3614fee8be8d9981
Leandro Gabriel <github@lagabriel.com>
2026-02-12 18:27:54 +0000

    Atualiza dependências para versões mais recentes no `bun.lock` e renomeia workspace para `CESAE_Mobile`.

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,6 @@ import {customLogger} from "./CustomLogger";
 
 type Variables = JwtVariables
 
-// const app = new Hono()
 const app = new Hono<{ Variables: Variables }>()
 
 const db = drizzle(process.env.DATABASE_URL!);
@@ -89,27 +88,7 @@ app.post('/admin/run-scrape', async c => {
 
     return c.json({status: 'ok', message: 'Scrape started successfully.'});
 });
-// TODO: Proteger este endpoint (token, JWT, IP whitelist, etc.)
-// app.post('/admin/run-scrape', async c => {
-//     customLogger("INFO", "Scrape started by admin request...")
-//
-//     job?.trigger();
-//
-//     return c.json({ status: 'ok', message: 'Scrape started successfully.' });
-// });
 
-// TODO: Proteger este endpoint (token, JWT, IP whitelist, etc.)
-// app.get('/admin/scrape-job-info', async c => {
-//     customLogger("INFO", "Scrape info requested by admin...")
-//
-//     const jobData = {
-//         lastRun: job?.currentRun(),
-//         nextRun: job?.nextRun(),
-//         isActive: job?.isStopped()
-//     }
-//
-//     return c.json({ data: jobData, status: 'ok' });
-// });
 app.get('/admin/scrape-job-info', async c => {
     customLogger("INFO", "Scrape info requested by admin...")
 
@@ -121,6 +100,7 @@ app.get('/admin/scrape-job-info', async c => {
 
     return c.json({data: jobData, status: 'ok'});
 });
+
 // app.get('/courses/:id', async (c) => {
 //     const id = Number(c.req.param('id'))
 //     const course = await db.select().from(courses).where(eq(courses.id, id));
@@ -146,18 +126,5 @@ app.get('/courses', async (c) => {
         return c.json({error: 'Internal Server Error'}, {status: 500});
     }
 })
-
-// app.get('/courses/:id', async (c) => {
-//     const id = Number(c.req.param('id'))
-//     const course = await db.select().from(courses).where(eq(courses.id, id));
-//
-//     if (course[0] !== undefined) {
-//         customLogger('INFO', `Getting course id ${id} data from the database.`)
-//         return c.json(course, {status: 200})
-//     }
-//
-//     customLogger('ERROR', `Course ${id} not found.`)
-//     return c.json({error: 'Course not found'}, {status: 404})
-// })
 
 export default app
