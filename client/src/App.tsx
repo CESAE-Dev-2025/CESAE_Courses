731353e3dcd4dcaaf0912d537755efd7e0b6dcef
AriciaLima <ariciafariasl@gmail.com>
2026-02-18 14:17:25 +0000

    add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,77 +1,21 @@
-import {useState} from 'react'
-import beaver from './assets/beaver.svg'
-import './App.css'
-import {Course} from "shared/dist/types/course";
-
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import HeroBanner from "@/components/HeroBanner/HeroBanner";
+import CourseList from "@/components/CourseList/CourseList";
+import coursesData from "@/courses_v2.json";
+import heroBg from "@/assets/bg-cesae-hero.jpg";
 
 function App() {
-    const [coursesData, setCoursesData] = useState<Course[] | undefined>()
-    // const [courseData, setCourseData] = useState<Course | undefined>()
-
-    async function getCourses() {
-        try {
-            const req = await fetch(`${SERVER_URL}/courses`)
-            const res: Course[] = await req.json()
-            setCoursesData(res)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    /* Função para buscar 1 curso (por id)
-    async function getCourse(courseId: number) {
-
-        try {
-            const req = await fetch(`${SERVER_URL}/courses/${courseId}`)
-            const res: Course = await req.json()
-            setCourseData(res)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    */
-
-    return (
-        <>
-            <div>
-                <a href="https://github.com/stevedylandev/bhvr" target="_blank">
-                    <img src={beaver} className="logo" alt="beaver logo"/>
-                </a>
-            </div>
-            <h1>bhvr</h1>
-            <h2>Bun + Hono + Vite + React</h2>
-            <p>A typesafe fullstack monorepo</p>
-            <div className="card">
-                <div className='button-container'>
-                    {/*<button onClick={() => getCourse(1)}>*/}
-                    {/*    Call API (curso 1)*/}
-                    {/*</button>*/}
-                    <button onClick={getCourses}>
-                        Call API (cursos)
-                    </button>
-                    <a className='docs-link' target='_blank' href="https://bhvr.dev">Docs</a>
-                </div>
-                {/*{courseData && (*/}
-                {/*    <pre className='response'>*/}
-                {/*        <code>*/}
-                {/*            Curso: {courseData.name} <br/>*/}
-                {/*            Início: {courseData.startDate} <br/>*/}
-                {/*            Horário: {courseData.time} / {courseData.timeDescription}<br/>*/}
-                {/*            Duração: {courseData.duration}*/}
-                {/*        </code>*/}
-                {/*  </pre>*/}
-                {/*)}*/}
-                {coursesData && (
-                    <pre className='response'>
-                        <code>
-                            Número de cursos: {coursesData.length}
-                        </code>
-                  </pre>
-                )}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <HeroBanner title="Nossos Cursos" backgroundImage={heroBg} />
+      <CourseList courses={coursesData} />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
