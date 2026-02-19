1f7894aa1d6034c534c9bbe5df39de52465f2e7a
Sandro-Draeger <sandrodraegerpt@gmail.com>
2026-02-19 18:45:47 +0000

    ajuste no elemento AboutObjectives

--- client/src/components/AboutObjectives/AboutObjectives.tsx
+++ client/src/components/AboutObjectives/AboutObjectives.tsx
@@ -1,5 +1,20 @@
 import styles from "./AboutObjectives.module.css";
 
+const objectives = [
+    {
+        id: "1",
+        text: "Responder às necessidades de formação designadamente nas áreas da economia digital e (re)qualificação profissional de ativos empregados e desempregados.",
+    },
+    {
+        id: "2",
+        text: "Potenciar o desenvolvimento das competências digitais, enquanto instrumento de promoção da empregabilidade dos ativos.",
+    },
+    {
+        id: "3",
+        text: "Promover a conceção e participação em percursos formativos qualificantes de jovens desempregados, com o objetivo de melhorar a sua empregabilidade.",
+    },
+];
+
 const AboutObjectives = () => {
     return (
         <section className={styles.objectivesSection}>
@@ -7,31 +22,12 @@ const AboutObjectives = () => {
                 <h2 className={styles.title}>Objetivos</h2>
 
                 <div className={styles.cards}>
-                    <div className={styles.card}>
-                        <span className={styles.number}>01</span>
-                        <p className={styles.text}>
-                            Responder às necessidades de formação designadamente nas áreas
-                            da economia digital e (re)qualificação profissional de ativos
-                            empregados e desempregados.
-                        </p>
-                    </div>
-
-                    <div className={styles.card}>
-                        <span className={styles.number}>02</span>
-                        <p className={styles.text}>
-                            Potenciar o desenvolvimento das competências digitais,
-                            enquanto instrumento de promoção da empregabilidade dos ativos.
-                        </p>
-                    </div>
-
-                    <div className={styles.card}>
-                        <span className={styles.number}>03</span>
-                        <p className={styles.text}>
-                            Promover a conceção e participação em percursos formativos
-                            qualificantes de jovens desempregados, com o objetivo de
-                            melhorar a sua empregabilidade.
-                        </p>
-                    </div>
+                    {objectives.map((objective) => (
+                        <div key={objective.id} className={styles.card}>
+                            <span className={styles.backgroundNumber}>{objective.id}</span>
+                            <p className={styles.text}>{objective.text}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
