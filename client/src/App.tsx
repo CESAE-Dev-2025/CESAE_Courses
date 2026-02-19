b395f39b8b6f629beafc4da98575d495252c6b52
Sandro-Draeger <sandrodraegerpt@gmail.com>
2026-02-19 16:51:34 +0000

    ajustments on scrapper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
             <Header />
 
             <Routes>
-                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/about" element={<AboutMe />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
