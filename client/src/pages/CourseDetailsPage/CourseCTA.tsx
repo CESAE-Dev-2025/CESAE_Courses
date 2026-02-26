ccb815156871277fe0cc552112a9477326e1e54b
Leandro Gabriel <github@lagabriel.com>
2026-02-26 18:34:59 +0000

    Adiciona suporte ao download de documentos no `CourseCTA`.

--- client/src/pages/CourseDetailsPage/CourseCTA.tsx
+++ client/src/pages/CourseDetailsPage/CourseCTA.tsx
@@ -1,5 +1,5 @@
-import { MDBBtn } from "mdb-react-ui-kit";
-import { Course } from "shared";
+import {MDBBtn} from "mdb-react-ui-kit";
+import {Course} from "shared";
 import styles from "./CourseDetailsPage.module.css";
 
 interface Props {
@@ -10,7 +10,7 @@ function hasText(value?: string | null) {
     return Boolean(value?.trim());
 }
 
-export default function CourseCTA({ course }: Props) {
+export default function CourseCTA({course}: Props) {
     const hasEnrollmentUrl =
         hasText(course.enrollment) &&
         (course.enrollment.startsWith("http://") ||
@@ -18,24 +18,33 @@ export default function CourseCTA({ course }: Props) {
 
     const shouldShowDownloadButton = Boolean(course.hasDownloadButton);
 
-/*    function downloadDoc(idDoc?: string | number) {
+    function downloadDoc(idDoc?: string | number) {
         if (!idDoc) return;
 
         const form = document.getElementById("frmDown") as HTMLFormElement | null;
         if (!form) return;
 
-        const idDocInput =
-            form.querySelector<HTMLInputElement>('input[name="idDoc"]');
+        const idDocInput = form.querySelector<HTMLInputElement>('input[name="idDoc"]');
         if (!idDocInput) return;
 
         idDocInput.value = String(idDoc);
         form.submit();
-    }*/
+    }
 
     if (!hasEnrollmentUrl && !shouldShowDownloadButton) return null;
 
     return (
         <div className={styles.mobileCta}>
+            <form
+                name="frmDown"
+                id="frmDown"
+                method="post"
+                action="https://cesaedigital.pt/ifldr/guestDownloader.aspx"
+                target="_blank"
+            >
+                <input type="hidden" id="idDoc" name="idDoc"/>
+                <input type="hidden" id="DownMode" name="DownMode" value="TIMED"/>
+            </form>
             {hasEnrollmentUrl && (
                 <MDBBtn
                     tag="a"
@@ -52,10 +61,30 @@ export default function CourseCTA({ course }: Props) {
                 <MDBBtn
                     type="button"
                     className="btn-brand-secondary w-100"
-                   // onClick={() => downloadDoc(course.downloadId)}
+                    onClick={() => downloadDoc(course.downloadId)}
                 >
                     Descarregar Programa
                 </MDBBtn>
+
+                // <button
+                //     className="btn btn-brand-secondary w-100"
+                //     onClick={(event) => {
+                //         event.preventDefault()
+                //         const frm = document.getElementById("frmDown");
+                //
+                //         const idDocInput = frm!.querySelector<HTMLInputElement>('input[name="idDoc"]');
+                //
+                //         idDocInput!.value = course.downloadId
+                //
+                //         console.log(course.downloadId)
+                //         console.log(idDocInput!.value)
+                //
+                //         frm!.submit();
+                //     }}
+                // >
+                //     Descarregar Programa TESTE
+                // </button>
+
             )}
         </div>
     );
